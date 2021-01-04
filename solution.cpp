8b117b24e42823708454382b9b5f29f8200335bb
Al-Mahmud-2019331048 <76640589+Al-Mahmud-2019331048@users.noreply.github.com>
2021-01-04 12:02:42 +0600

    Template solution
    Students should have to write their solution in this file.

---
+++ solution.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int fibo(int n)
+{
+ int ret;
+ //your code goes here
+ return ret;
+}
+
+int main()
+{
+ int n;
+ cin>>n;
+ cout<<fibo(n)<<endl;
+ return o;
+}
